70c99dbca84f1c796ef1004a53768dce680c834b
Ali Rezai <ali.rezai27461@gmail.com>
2025-06-03 11:31:40 +0200

    Initial commit

---
+++ src/main/java/se/mozaffary/jsonxmlconverterapi/JsonXmlConverterApiApplication.java
@@ -0,0 +1,13 @@
+package se.mozaffary.jsonxmlconverterapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonXmlConverterApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsonXmlConverterApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/mozaffary/jsonxmlconverterapi/JsonXmlConverterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package se.mozaffary.jsonxmlconverterapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonXmlConverterApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
